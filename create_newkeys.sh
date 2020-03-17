1e145a75955e0191cc5df87aab6252a3edab4444
josefcoap <53934875+josefcoap@users.noreply.github.com>
2020-03-17 07:47:54 +0100

    Script to help people generate new keys
    It is deisgned to use it on every server, one by one. It prompts you about how many node you have. I'm sure it can be improved, feel free to adjust it or improve it.

---
+++ create_newkeys.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+set -e
+
+echo -e
+echo 'This script is to generate your new keys, be sure you have your repos at least on version 1.0.94.'
+echo -e
+echo 'Use this script only once time in the same machine'
+
+read -p "What's your keys folder name? (default VALIDATOR_KEYS) : " FOLDERNAME
+if [ "$FOLDERNAME" = "" ]
+  then
+      FOLDERNAME="VALIDATOR_KEYS"
+  fi
+
+echo 'you have choose '$FOLDERNAME
+
+if [ -d "$FOLDERNAME" ]; then
+  echo "Folder ${FOLDERNAME} found ..."
+	OLD_COPY=$FOLDERNAME"_old"
+	echo -e
+	echo "A backup of your old keys will be moved to $OLD_COPY"
+	if [ -d "$OLD_COPY" ]; then
+		echo "Error: $OLD_COPY found. You had executed this two times. Can not continue."
+	else
+		echo "Proceding with the key generation..."
+		read -p "How many keys do you want to generate ? : " NUMBEROFNODES
+		  re='^[0-9]+$'
+		  if ! [[ $NUMBEROFNODES =~ $re ]] && [ "$NUMBEROFNODES" -gt 0 ]
+		  then
+		      NUMBEROFNODES = 1
+		  fi
+
+		cd $HOME
+		mv $FOLDERNAME $OLD_COPY 
+		mkdir $FOLDERNAME
+
+		cd $HOME/elrond-utils
+
+		for i in $(seq 1 $NUMBEROFNODES); 
+			do 
+				INDEX=$(( $i - 1 ))
+				./keygenerator
+				cp *.pem $HOME/elrond-nodes/node-$INDEX/config
+				zip node-$INDEX.zip *.pem
+				mv node-$INDEX.zip $HOME/$FOLDERNAME/
+				rm *.pem 
+			done
+
+	fi
+else
+  echo "Error: ${FOLDERNAME} not found. Can not continue."
+  exit 1
+fi
+
+
+
